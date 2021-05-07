5fca363333b9cc496978e19ac40f9a48cf06c6e2
Jane Vo <vo.janehanh@gmail.com>
2021-05-07 00:09:03 -0700

    Milestone 1 methods

---
+++ index.py
@@ -0,0 +1,32 @@
+# methods for index creation
+
+# inverted index
+# token is the key, the value is a list that contains the urls and its frequency
+
+from collections import defaultdict
+import sys
+import json
+
+
+# index = {token: {url: freq, url:freq}, token: {url: freq, url: freq}}
+
+index = defaultdict(dict)
+
+def inverted_index(url, data, index_file):
+    for token in data:
+        index[token][url] = data[token]
+
+    check_size(index_file)
+
+def check_size(index_file):
+    global index
+    size = sys.getsizeof(index)
+    if size > 500000:
+        print('writing')
+        json.dump(index, index_file, indent=3)
+        index.clear()
+
+def write_remaining(index_file):
+    global index
+    json.dump(index, index_file, indent = 3)
+    index.clear()
